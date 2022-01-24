a2fac37fb1a9e1106ffcf039ec4f635f279aeced
Otim Fredrick <fredrickot@gmail.com>
2022-01-24 12:53:36 +0300

    adds simple open auction contract

--- contracts/Voting/Ballot.sol
+++ contracts/Voting/Ballot.sol
@@ -1,7 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-/// @title Voting with delegation.
+/**
+The idea is to create one contract per ballot, providing a short name for each option. 
+Then the creator of the contract who serves as chairperson will give the right to vote to each address individually.
+
+The persons behind the addresses can then choose to either vote themselves or to delegate their vote to a person they trust.
+
+At the end of the voting time, winningProposal() will return the proposal with the largest number of votes.
+*/
+
 contract Ballot {
     
     // Represents a single voter.
---
+++ contracts/auction/SimpleAuction.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+/**
+Everyone can send their bids during a bidding period. 
+The bids already include sending money / Ether in order to bind the bidders to their bid. 
+If the highest bid is raised, the previous highest bidder gets their money back. 
+After the end of the bidding period, the contract has to be called manually 
+for the beneficiary to receive their money - contracts cannot activate themselves.
+*/
+
+contract SimpleAuction {
+    // Parameters of the auction. Times are either
+    // absolute unix timestamps (seconds since 1970-01-01)
+    // or time periods in seconds.
+    address payable public beneficiary;
+    uint public auctionEndTime;
+
+    // Current state of the auction.
+    address public highestBidder;
+    uint public highestBid;
+
+    // Allowed withdrawals of previous bids
+    mapping(address => uint) pendingReturns;
+
+    // Set to true at the end, disallows any change.
+    // By default initialized to `false`.
+    bool ended;
+
+    // Events that will be emitted on changes.
+    event HighestBidIncreased(address bidder, uint amount);
+    event AuctionEnded(address winner, uint amount);
+
+    // Errors that describe failures.
+    // The triple-slash comments are so-called natspec
+    // comments. They will be shown when the user
+    // is asked to confirm a transaction or
+    // when an error is displayed.
+
+    /// The auction has already ended.
+    error AuctionAlreadyEnded();
+    /// There is already a higher or equal bid.
+    error BidNotHighEnough(uint highestBid);
+    /// The auction has not ended yet.
+    error AuctionNotYetEnded();
+    /// The function auctionEnd has already been called.
+    error AuctionEndAlreadyCalled();
+
+    /// Create a simple auction with `biddingTime`
+    /// seconds bidding time on behalf of the
+    /// beneficiary address `beneficiaryAddress`.
+    constructor(uint biddingTime, address payable beneficiaryAddress) {
+        beneficiary = beneficiaryAddress;
+        auctionEndTime = block.timestamp + biddingTime;
+    }
+
+    /// Bid on the auction with the value sent
+    /// together with this transaction.
+    /// The value will only be refunded if the
+    /// auction is not won.
+    function bid() external payable {
+        if (block.timestamp > auctionEndTime)
+            revert AuctionAlreadyEnded();
+
+        if (msg.value <= highestBid)
+            revert BidNotHighEnough(highestBid);
+        
+        if (highestBid != 0) {
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit HighestBidIncreased(msg.sender, msg.value);
+    }
+
+    /// Withdraw a bid that was overbid.
+    function withdraw() external returns(bool) {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            pendingReturns[msg.sender] = 0;
+
+            if (!payable(msg.sender).send(amount)) {
+                pendingReturns[msg.sender] = amount;
+                return false;
+            } 
+        } 
+        return true;
+    }
+
+    /// End the auction and send the highest bid
+    /// to the beneficiary.
+    function auctionEnd() external {
+        // It is a good guideline to structure functions that interact
+        // with other contracts (i.e. they call functions or send Ether)
+        // into three phases:
+        // 1. checking conditions
+        // 2. performing actions (potentially changing conditions)
+        // 3. interacting with other contracts
+
+        // 1. Condition
+        if (block.timestamp < auctionEndTime)
+            revert AuctionNotYetEnded();
+        if (ended)
+            revert AuctionEndAlreadyCalled();
+        
+        // 2. Effects
+        ended = true;
+        emit AuctionEnded(highestBidder, highestBid);
+
+        // 3. Interaction
+        beneficiary.transfer(highestBid);
+    }
+}
